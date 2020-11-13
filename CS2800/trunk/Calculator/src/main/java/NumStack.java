17ac18fcb0b5d663c150bb3eff9e30beb5975f9f
Zhac109 <Zhac109@04181e28-4d87-4ec2-a922-a4c460fd4885>
2020-11-13 00:05:22 +0000

    Merged changes from branch TestOpStack

--- CS2800/trunk/Calculator/src/main/java/NumStack.java
+++ CS2800/trunk/Calculator/src/main/java/NumStack.java
@@ -5,17 +5,54 @@
  * @author zhac109
  */
 public class NumStack {
-  private Stack stack = new Stack();
-  
+  private Stack numStack = new Stack();
+
   Entry entry;
-  
+
+  int size = 0;
+
   /**
-   * Returns the size of the stack.
+   * Pushes a float onto the stack.
    * 
-   * @return the size of the stack
+   * @param i The float to be pushed onto the stack.
    */
-  public int size() {
-    return stack.size();
+
+  public void push(double i) {
+    entry = new Entry(i);
+    numStack.push(entry);
+    size = numStack.size;
   }
-  
+
+  /**
+   * Returns the float on the top of the stack.
+   * 
+   * @return the last floated pushed to the stack.
+   * @throws BadTypeException if the last entry pushed to the stack was not a number.
+   */
+
+  public double pop() throws BadTypeException {
+    entry = numStack.pop();
+    if (entry.getType() == "Number") {
+      double val = entry.getValue();
+      size = numStack.size();
+      return val;
+    } else {
+      throw new BadTypeException(null);
+    }
+  }
+
+  /**
+   * If the stack is empty, returns true. Else returns false.
+   * 
+   * @return the boolean depending on whether or not the stack is empty.
+   */
+
+  public Boolean isEmpty() {
+    if (size == 0) {
+      return true;
+    }
+    return false;
+  }
+
+
 }
--- CS2800/trunk/Calculator/src/test/java/TestNumStack.java
+++ CS2800/trunk/Calculator/src/test/java/TestNumStack.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.EmptyStackException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -10,15 +12,60 @@ class TestNumStack {
 
   public NumStack stack;
 
-  Entry entry;
+  double i = 5;
+
+  @BeforeEach
+  public void setup() {
+    stack = new NumStack();
+  }
 
   /**
-   * Test 1. Tests that the compositing of the stack class into the numStack works correctly.
+   * Test 1. Tests the push method works. For this I created and wrote the push method.
+   */
+
+  @Test
+  void testPush() {
+    stack.push(i);
+    assertEquals(stack.size, 1, "The size of the stack should be 1");
+  }
+
+  /**
+   * Test 2. Tests the pop method works. For this I created and wrote the pop method.
+   * 
+   * @throws BadTypeException if the top entry of the stack is not a number.
+   */
+
+  @Test
+  void testPop() throws BadTypeException {
+    stack.push(i);
+    assertEquals(stack.pop(), 5, "The value popped should be the last value pushed onto the stack");
+  }
+
+  /**
+   * Test 3. Tests that the isEmpty method works. For this I created and wrote the IsEmpty method.
    */
   @Test
-  void testComposition() {
-    stack = new NumStack();
-    assertEquals(stack.size(), 0, "The size of the stack should be 0");
+  void testIsEmpty() {
+    assertTrue(stack.isEmpty());
+    stack.push(i);
+    assertFalse(stack.isEmpty());
+  }
+
+  /**
+   * Test 4. Tests whether popping an empty stack throws an empty stack or not. Mainly to test
+   * whether or not I needed to check for it in the NumStack class or if the Stack class would cover
+   * it. For this I did not need to code anything new as the Stack class did cover it.
+   * 
+   * 
+   * @throws BadTypeException if the Entry on the top of the stack is not a number.
+   */
+
+  @Test
+  void testEmptyPop() throws BadTypeException {
+    stack.push(i);
+    stack.pop();
+    assertThrows(EmptyStackException.class, () -> stack.pop(),
+        "Pushing and then popping twice should throw an exception");
   }
 
 }
