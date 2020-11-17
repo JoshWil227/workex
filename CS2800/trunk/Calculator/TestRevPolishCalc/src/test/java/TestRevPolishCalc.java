74b941b6cbabf2265884b56404d0120899c1ad9a
Zhac109 <Zhac109@04181e28-4d87-4ec2-a922-a4c460fd4885>
2020-11-17 23:52:44 +0000

    Write Test 2, TestTwoNums, which tests that RevPolishCalc.evaluate() throws an exception if passed two numbers and no operand.

--- CS2800/trunk/Calculator/TestRevPolishCalc/src/test/java/TestRevPolishCalc.java
+++ CS2800/trunk/Calculator/TestRevPolishCalc/src/test/java/TestRevPolishCalc.java
@@ -10,15 +10,28 @@ class TestRevPolishCalc {
 
   private RevPolishCalc calc = new RevPolishCalc();
   
+  String s;
+  
+  double d;
+  
   /**
    * Test 1. Tests that the evaluate method returns the number passed into it when only one number is entered.
    * For this I made it parse the string into a double and return said double.
    */
   @Test
   void testOneNum() {
-    String s = "5";
-    double d = calc.evaluate(s);
+    s = "5";
+    d = calc.evaluate(s);
     assertEquals(d, 5, "The single number returned should be the same as the one entered.");
+  }
+  
+  /**
+   * Test 2. Tests that the evaluate method throws an InvalidExpression exception if passed two numbers and no operand.
+   */
+  @Test
+  void testTwoNums() {
+   s = "5 6"; 
+   assertThrows(InvalidExpression.class, () -> calc.evaluate(s));
   }
 
 }
