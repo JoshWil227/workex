0ca37b421e0a3e9785e64a93b2c2d7e55e877c03
Zhac109 <Zhac109@04181e28-4d87-4ec2-a922-a4c460fd4885>
2020-11-17 21:33:45 +0000

    Wrote the TestOneNum test, which tests that the evaluate method returns the value entered.

--- CS2800/trunk/Calculator/TestRevPolishCalc/src/test/java/TestRevPolishCalc.java
+++ CS2800/trunk/Calculator/TestRevPolishCalc/src/test/java/TestRevPolishCalc.java
@@ -1,11 +1,20 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
+/**
+ * The test class for the RevPolishCalc class.
+ * 
+ * @author zhac109
+ */
 class TestRevPolishCalc {
 
+  private RevPolishCalc calc = new RevPolishCalc();
+  
   @Test
-  void test() {
-    fail("Not yet implemented");
+  void testOneNum() {
+    String s = "5";
+    float f = calc.evaluate(s);
+    assertEquals(f, 5, "The single number returned should be the same as the one entered.");
   }
 
 }
