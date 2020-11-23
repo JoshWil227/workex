383227412c8f5688332617117ed54732ac72c70a
Zhac109 <Zhac109@04181e28-4d87-4ec2-a922-a4c460fd4885>
2020-11-23 18:36:41 +0000

    TestStandardCalc Branch. Wrote Test2, testOneOp, testing that the StandardCalc evaluate method correctly handles one-clause infix expressions. Also refactored code, making the string and double global variables.

--- CS2800/trunk/Calculator/TestStandardCalc/src/test/java/TestStandardCalc.java
+++ CS2800/trunk/Calculator/TestStandardCalc/src/test/java/TestStandardCalc.java
@@ -8,16 +8,30 @@ import org.junit.jupiter.api.Test;
  */
 class TestStandardCalc {
 
+  StandardCalc calc = new StandardCalc();
+  String s;
+  Double d;
+  
   /**
    * Test 1. Tests that the calculator returns single-number inputs as they were entered.
    * For this I created the evaluate method and had it return the number entered.
    */
   @Test
   void testOneNum() {
-    StandardCalc calc = new StandardCalc();
-    String d = "5";
-    calc.evaluate(d);
-    assertEquals(d, "5", "The value returned should be the value entered.");
+    s = "5";
+    d = calc.evaluate(s);
+    assertEquals(d, 5, "The value returned should be the value entered.");
+  }
+  
+  
+  /**
+   * Test 2. Tests that the calculator correctly handles single-clause infix expressions.
+   */
+  @Test
+  void testOneOp() {
+    s = "5 + 5";
+    calc.evaluate(s);
+    assertEquals(d, 10, "The value returned should be the result of the expression entered.");
   }
 
 }
