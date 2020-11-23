1873eeeb19b710f8583224142d3d8612db183ac5
Zhac109 <Zhac109@04181e28-4d87-4ec2-a922-a4c460fd4885>
2020-11-23 18:23:58 +0000

    TestStandardCalc Branch. Altered StandardCalc class to include an evaluate method which returns the string entered into it as a Double.

--- CS2800/trunk/Calculator/TestStandardCalc/src/main/java/StandardCalc.java
+++ CS2800/trunk/Calculator/TestStandardCalc/src/main/java/StandardCalc.java
@@ -6,4 +6,13 @@
  */
 public class StandardCalc {
 
+  /**
+   * Evaluates the user-entered expression passed into it in infix notation.
+   * 
+   * @param what the user-entered expression.
+   * @return the answer to the expression.
+   */
+  public double evaluate(String what) {
+    return Double.parseDouble(what);
+  }
 }
