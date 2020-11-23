fc99a0c628c5f2f33e40f68291653d0ed741ddb9
Zhac109 <Zhac109@04181e28-4d87-4ec2-a922-a4c460fd4885>
2020-11-23 17:42:18 +0000

    Created TestStandardCalc and StandardCalc classes & added their javadoc comments

---
+++ CS2800/trunk/Calculator/TestStandardCalc/src/main/java/StandardCalc.java
@@ -0,0 +1,9 @@
+
+/**
+ * Evaluates expressions in Infix notation.
+ * 
+ * @author zhac109
+ */
+public class StandardCalc {
+
+}
---
+++ CS2800/trunk/Calculator/TestStandardCalc/src/test/java/TestStandardCalc.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests the StandardCalc class.
+ * 
+ * @author zhac109
+ */
+class TestStandardCalc {
+
+  @Test
+  void test() {
+    fail("Not yet implemented");
+  }
+
+}
