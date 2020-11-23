714c2c0f7729268cc4ee4b4d6eb48bee17d9c01f
Zhac109 <Zhac109@04181e28-4d87-4ec2-a922-a4c460fd4885>
2020-11-23 19:15:56 +0000

    TestStandardCalc Branch. Altered StandardCalc's evaluate method to make the evaluate method split the expression, then add the numbers to a new string and the operator to a different string, which is then added on at the end. That string is then passed into the RevPolishCalc's evaluate method.

--- CS2800/trunk/Calculator/TestStandardCalc/src/main/java/StandardCalc.java
+++ CS2800/trunk/Calculator/TestStandardCalc/src/main/java/StandardCalc.java
@@ -6,13 +6,33 @@
  */
 public class StandardCalc {
 
+  RevPolishCalc calc = new RevPolishCalc();
+
   /**
    * Evaluates the user-entered expression passed into it in infix notation.
    * 
    * @param what the user-entered expression.
    * @return the answer to the expression.
+   * @throws InvalidExpression if an invalid expression is passed in by the user.
+   * @throws BadTypeException if the method tries to push the wrong type onto a stack.
    */
-  public double evaluate(String what) {
-    return Double.parseDouble(what);
+  public double evaluate(String what) throws InvalidExpression, BadTypeException {
+    String[] inputs = what.split(" ");
+    String op = "";
+    String revPol = "";
+    if (inputs.length == 1) {
+      return Double.parseDouble(what);
+    }
+    for (int i = 0; i < inputs.length; i++) {
+      String token = inputs[i];
+      if (token.equals("+")) {
+        op = "+";
+      } else {
+        revPol = revPol + token + " ";
+      }
+    }
+    revPol = revPol + op;
+    double ans = calc.evaluate(revPol);
+    return ans;
   }
 }
--- CS2800/trunk/Calculator/TestStandardCalc/src/test/java/TestStandardCalc.java
+++ CS2800/trunk/Calculator/TestStandardCalc/src/test/java/TestStandardCalc.java
@@ -11,26 +11,35 @@ class TestStandardCalc {
   StandardCalc calc = new StandardCalc();
   String s;
   Double d;
-  
+
   /**
-   * Test 1. Tests that the calculator returns single-number inputs as they were entered.
-   * For this I created the evaluate method and had it return the number entered.
+   * Test 1. Tests that the calculator returns single-number inputs as they were entered. For this I
+   * created the evaluate method and had it return the number entered.
+   * 
+   * @throws InvalidExpression if an invalid expression is passed in by the users.
+   * @throws BadTypeException if the method tries to push a symbol instead of a number.
    */
   @Test
-  void testOneNum() {
+  void testOneNum() throws InvalidExpression, BadTypeException {
     s = "5";
     d = calc.evaluate(s);
     assertEquals(d, 5, "The value returned should be the value entered.");
   }
-  
-  
+
+
   /**
-   * Test 2. Tests that the calculator correctly handles single-clause infix expressions.
+   * Test 2. Tests that the calculator correctly handles single-clause infix expressions. For this I
+   * made the evaluate method split the expression, then add the numbers to a new string and the
+   * operator to a different string, which is then added on at the end. That string is then passed
+   * into the RevPolishCalc's evaluate method.
+   * 
+   * @throws InvalidExpression if an invalid expression is passed in by the users.
+   * @throws BadTypeException if the method tries to push a symbol instead of a number.
    */
   @Test
-  void testOneOp() {
+  void testOneOp() throws InvalidExpression, BadTypeException {
     s = "5 + 5";
-    calc.evaluate(s);
+    d = calc.evaluate(s);
     assertEquals(d, 10, "The value returned should be the result of the expression entered.");
   }
 
