ebd22a5ef72741f3d75604e44cdb5fd37cd7e9c0
Zhac109 <Zhac109@04181e28-4d87-4ec2-a922-a4c460fd4885>
2020-11-23 18:25:47 +0000

    TestStandardCalc Branch. Added Test1 javadoc comments.

--- CS2800/trunk/Calculator/TestStandardCalc/src/test/java/TestStandardCalc.java
+++ CS2800/trunk/Calculator/TestStandardCalc/src/test/java/TestStandardCalc.java
@@ -10,12 +10,14 @@ class TestStandardCalc {
 
   /**
    * Test 1. Tests that the calculator returns single-number inputs as they were entered.
+   * For this I created the evaluate method and had it return the number entered.
    */
   @Test
   void testOneNum() {
     StandardCalc calc = new StandardCalc();
     String d = "5";
     calc.evaluate(d);
+    assertEquals(d, "5", "The value returned should be the value entered.");
   }
 
 }
