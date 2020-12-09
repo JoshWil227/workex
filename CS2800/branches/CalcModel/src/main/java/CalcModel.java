e401bbca88d84cdbae60963ef4f954bff3a2ff93
Zhac109 <Zhac109@04181e28-4d87-4ec2-a922-a4c460fd4885>
2020-12-09 22:41:24 +0000

    CalcModel Branch. Wrote test 2, which tests that the evaluate method works correctly for both notations. I also added the evaluate method to CalcModel, which calls the evaluate method of either the RevPolishCalc instance or the StandardCalc instance, depending on the value of the boolean.

--- CS2800/branches/CalcModel/src/main/java/CalcModel.java
+++ CS2800/branches/CalcModel/src/main/java/CalcModel.java
@@ -7,4 +7,21 @@
 public class CalcModel {
   Calculator revPolishCalc = new RevPolishCalc();
   Calculator standardCalc = new StandardCalc();
+
+  /**
+   * Evaluates the expression passed into it in the notation given.
+   * 
+   * @param string The expression to be evaluated.
+   * @param b The boolean which decides which notation is to be used.
+   * @return The answer to the expression,
+   * @throws InvalidExpression if an invalid expression is passed in.
+   * @throws BadTypeException if the method tries to pop a symbol instead of a number.
+   */
+  public double evaluate(String string, boolean b) throws InvalidExpression, BadTypeException {
+    if (b == true) {
+      return standardCalc.evaluate(string);
+    } else {
+      return revPolishCalc.evaluate(string);
+    }
+  }
 }
--- CS2800/branches/CalcModel/src/test/java/TestCalcModel.java
+++ CS2800/branches/CalcModel/src/test/java/TestCalcModel.java
@@ -9,14 +9,31 @@ import org.junit.jupiter.api.Test;
 class TestCalcModel {
 
   /**
-   * Test 1. Tests that the Model correctly implements the two Calculator interfaces correctly.
-   * For this I 
+   * Test 1. Tests that the Model correctly implements the two Calculator interfaces correctly. For
+   * this I had the CalcModel initialise the two Calculator interfaces.
    */
   @Test
   void testHasCalcs() {
     CalcModel model = new CalcModel();
-    assertFalse(model.revPolishCalc == null);
-    assertFalse(model.standardCalc == null);
+    assertFalse(model.revPolishCalc == null, "The calculator should be initialised.");
+    assertFalse(model.standardCalc == null, "The calculator should be initialised.");
+  }
+
+  /**
+   * Test 2. Tests that the evaluate method works correctly for both notations. For this I created
+   * the CalcModel evaluate method, and had it call the evaluate method of one of the two Calculator
+   * interfaces' methods depending on the value of the boolean.
+   * 
+   * @throws InvalidExpression if an invalid expression is passed in.
+   * @throws BadTypeException if the method tries to pop a symbol instead of a number.
+   */
+  @Test
+  void testEval() throws InvalidExpression, BadTypeException {
+    CalcModel model = new CalcModel();
+    assertEquals(model.evaluate("6 * 3", true), 18,
+        "The model should evaluate the expression in the correct notation.");
+    assertEquals(model.evaluate("6 3 *", false), 18,
+        "The model should evaluate the expression in the correct notation.");
   }
 
 }
