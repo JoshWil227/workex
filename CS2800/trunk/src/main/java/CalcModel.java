22a24adffae1fa714ec13d42642dcd081e593246
Zhac109 <Zhac109@04181e28-4d87-4ec2-a922-a4c460fd4885>
2020-12-10 13:53:25 +0000

    Merged CalcModel into trunk.

---
+++ CS2800/trunk/src/main/java/CalcModel.java
@@ -0,0 +1,27 @@
+
+/**
+ * This class is the model in which the controller will process information.
+ * 
+ * @author zhac109
+ */
+public class CalcModel {
+  Calculator revPolishCalc = new RevPolishCalc();
+  Calculator standardCalc = new StandardCalc();
+
+  /**
+   * Evaluates the expression passed into it in the notation given.
+   * 
+   * @param string The expression to be evaluated.
+   * @param infix The boolean which decides which notation is to be used.
+   * @return The answer to the expression,
+   * @throws InvalidExpression if an invalid expression is passed in.
+   * @throws BadTypeException if the method tries to pop a symbol instead of a number.
+   */
+  public double evaluate(String string, boolean infix) throws InvalidExpression, BadTypeException {
+    if (infix == true) {
+      return standardCalc.evaluate(string);
+    } else {
+      return revPolishCalc.evaluate(string);
+    }
+  }
+}
